0e997799f2c0d8c2794bfbf3b2b8bda71607e628
Jackie Ye <jackieye@Jackies-MacBook-Pro.local>
2019-05-16 14:50:19 -0700

    Create working mySQL database and tables

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
+-- run mysql -u root -p < schema.sql to create Database and tables
 DROP DATABASE IF EXISTS complete_menu;
 
-CREATE DATABASE copmplete_menu;
+CREATE DATABASE complete_menu;
 
 USE complete_menu;
 
@@ -8,13 +9,16 @@ CREATE TABLE menu (
   id INT NOT NULL AUTO_INCREMENT,
   main_description VARCHAR(255),
   single_menu_item VARCHAR(255),
-  price_per_guest INT
+  price_per_guest INT,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE menu_items (
   id INT NOT NULL AUTO_INCREMENT,
   item_name VARCHAR(255),
   item_description VARCHAR(255),
-  menu_item_id INT
-  FOREIGN KEY menu_item_id (menu_item_id) REFERENCES menu (id)
+  menu_item_id INT,
+  FOREIGN KEY (menu_item_id) REFERENCES menu (id),
+  PRIMARY KEY (id)
 );
+
