88c0f8d5a19d4a2bb90ae294ba46caf8fcde4e32
Jackie Ye <jackieye@Jackies-MBP.attlocal.net>
2019-05-22 22:56:27 -0700

    Add more server side tests

--- test/test.js
+++ test/test.js
@@ -31,4 +31,31 @@ beforeAll(async () => {
         done();
       });
     });
+
+    test('Response type should be an object', async(done) => {
+      request(app).get('/menu').then((response) => {
+        expect(typeof response).toBe('object');
+        done();
+      })
+    })
+
+    test('Get back error when encountering wrong endpoint', async(done) => {
+      request(app).get('/menu/menu').then((response) => {
+        expect(response.status).toBe(404);
+        done();
+      })
+    })
+
 });
+
+describe('Test the database schema', () => {
+  test('Contains correct columns', async(done) => {
+    let expected = ['id', 'main_description', 'single_menu', 'price_per_guest', 'dish_name', 'dish_description', 'single_menu_id'];
+    // expect.arrayContaining jest method
+    request(app).get('/menu').then((response) => {
+      expect(Object.keys(response)).toEqual(expect.arrayContaining(expected));
+      done();
+  })
+})
+
+})
